71a6a89972b0d6b472966a2818cb7bb30de22709
pervez72 <85360887+pervez72@users.noreply.github.com>
2021-07-19 10:02:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myintentdemo3/Activity1.java
@@ -0,0 +1,58 @@
+package com.example.myintentdemo3;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity1 extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.Button1);
+        textView=findViewById(R.id.textviewId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent=new Intent(Activity1.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+
+
+
+            }
+        });
+
+
+    }
+
+/*    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            String value=data.getStringExtra("key");
+            textView.setText(value);
+        }
+
+    }
+}
